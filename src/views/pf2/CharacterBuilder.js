904d6763f00d20966aafe628f47bd517f1c7e771
Joe Ziemba <jziemba90@gmail.com>
2020-01-20 16:42:01 -0500

    15 - abstracted migrations into separate file

--- src/views/pf2/CharacterBuilder.js
+++ src/views/pf2/CharacterBuilder.js
@@ -1,9 +1,9 @@
 import React from "react";
 import _ from "lodash";
 import { firebase } from "../../Firebase";
-import { Classes, ClassNames } from "../../_data/classes";
+import { Classes } from "../../_data/classes";
 import { Ancestries } from "../../_data/ancestries";
-// import { Abilities } from "../../_data/abilities";
+import * as Migrate from "../../migrations";
 import BUILDER_VERSION from "../../BUILDER_VERSION";
 
 import {
@@ -11,10 +11,8 @@ import {
   calculateAbilityScores,
   calculatePerception,
   getBlankCharacter,
-  upperLevelAbilityBoosts,
   calculateHP
 } from "../../_data/classTemplate";
-import { Proficiencies } from "../../_data/skills";
 import { Skills } from "../../_data/skills";
 import { Backgrounds } from "../../_data/backgrounds";
 import SubNav from "../../PF2CharacterBuilder/_components/SubNav";
@@ -74,80 +72,17 @@ class CharacterBuilder extends React.Component {
       character.uid = characterId;
 
       if (!character.builderVersion || character.builderVersion < "1.0.0") {
-        let blankCharacter = getBlankCharacter();
-
-        character.abilityBoosts = character.abilityBoosts.concat(
-          upperLevelAbilityBoosts
-        );
-        if (character.class.name) {
-          character.class = Classes[character.class.name];
-          if (!character.class.defenses) {
-            character.class.defenses = {
-              unarmored: Proficiencies.TRAINED
-            };
-          }
-        }
-
-        if (!character.feats || !Array.isArray(character.feats)) {
-          character.feats = blankCharacter.feats;
-        }
-
-        if (!character.saves) {
-          character.saves = blankCharacter.saves;
-        }
-
-        let oldBoosts = _.cloneDeep(character.skillBoosts);
-
-        oldBoosts = oldBoosts.filter(b => b.source === "Free");
-
-        character.skillBoosts = _.cloneDeep(blankCharacter.skillBoosts);
-
-        if (character.class.name) {
-          character.skillBoosts = character.skillBoosts.concat(
-            _.cloneDeep(character.class.skillBoosts)
-          );
-        }
-
-        if (character.background.name) {
-          character.skillBoosts = character.skillBoosts.concat(
-            _.cloneDeep(character.background.skillBoosts)
-          );
-        }
-
-        for (let i = 1; i <= character.abilityMods.Intelligence; i++)
-          character.skillBoosts.push({
-            id: "int" + i,
-            source: "int",
-            skill: { id: "Free" },
-            proficiency: 2
-          });
-
-        let freeClassBoosts = character.skillBoosts.filter(
-          b =>
-            (b.source === character.class.name || b.source === "int") &&
-            b.skill.id === "Free"
-        );
-
-        freeClassBoosts.forEach((boost, i) => {
-          if (oldBoosts[i]) boost.skill = oldBoosts[i].skill;
-        });
-
+        Migrate.v1_0_0(character);
         character.builderVersion = "1.0.0";
       }
 
       if (character.builderVersion < "1.0.1") {
-        character.abilityBoosts.forEach(boost => {
-          if (!boost.source.includes("_"))
-            boost.source = boost.source.replace("Level", "Level_");
-        });
+        Migrate.v1_0_1(character);
         character.builderVersion = "1.0.1";
       }
 
       if (character.builderVersion < "1.0.3") {
-        if (character.class.name === Classes.Rogue.name) {
-          character.skillBoosts = character.skillBoosts.concat(NEWROGUEBOOSTS);
-          character.feats = character.feats.concat(Classes.Rogue.feats);
-        }
+        Migrate.v1_0_3(character);
         character.builderVersion = "1.0.3";
       }
 
@@ -522,66 +457,3 @@ class CharacterBuilder extends React.Component {
 }
 
 export default CharacterBuilder;
-
-const NEWROGUEBOOSTS = [
-  {
-    skill: { id: "Free" },
-    source: ClassNames.ROGUE + "_2",
-    id: ClassNames.ROGUE + "9",
-    level: 2
-  },
-  {
-    skill: { id: "Free" },
-    source: ClassNames.ROGUE + "_4",
-    id: ClassNames.ROGUE + "10",
-    level: 4
-  },
-  {
-    skill: { id: "Free" },
-    source: ClassNames.ROGUE + "_6",
-    id: ClassNames.ROGUE + "11",
-    level: 6
-  },
-  {
-    skill: { id: "Free" },
-    source: ClassNames.ROGUE + "_8",
-    id: ClassNames.ROGUE + "12",
-    level: 8
-  },
-  {
-    skill: { id: "Free" },
-    source: ClassNames.ROGUE + "_10",
-    id: ClassNames.ROGUE + "13",
-    level: 10
-  },
-  {
-    skill: { id: "Free" },
-    source: ClassNames.ROGUE + "_12",
-    id: ClassNames.ROGUE + "14",
-    level: 12
-  },
-  {
-    skill: { id: "Free" },
-    source: ClassNames.ROGUE + "_14",
-    id: ClassNames.ROGUE + "15",
-    level: 14
-  },
-  {
-    skill: { id: "Free" },
-    source: ClassNames.ROGUE + "_16",
-    id: ClassNames.ROGUE + "16",
-    level: 16
-  },
-  {
-    skill: { id: "Free" },
-    source: ClassNames.ROGUE + "_18",
-    id: ClassNames.ROGUE + "17",
-    level: 18
-  },
-  {
-    skill: { id: "Free" },
-    source: ClassNames.ROGUE + "_20",
-    id: ClassNames.ROGUE + "18",
-    level: 20
-  }
-];
