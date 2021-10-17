951fbf83793c1a36302a1712b71c7d187c568d09
Joe Ziemba <jziemba90@gmail.com>
2021-10-16 22:23:41 -0700

    remove unused import

--- src/components/pfcb/FeatsSection.js
+++ src/components/pfcb/FeatsSection.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react"
+import React, { useState, useContext } from "react"
 import FeatEntry from "./FeatEntry"
 import { FeatSelection } from "./FeatSelection"
 import { Card } from "./Card"
